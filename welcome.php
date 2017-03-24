40a24e15161b688fabf4552f58ed4e1d65e82667
ASWANTH K <aswanthkoleri@users.noreply.github.com>
2017-03-24 19:41:42 +0530

    Added make new issue link to welcome page.

--- welcome.php
+++ welcome.php
@@ -1,3 +1,16 @@
+<?php
+	session_start();
+
+	if(!isset($_SESSION['login_user'])){  
+	  echo '<script language="javascript">';
+      echo 'alert("What? Dude login first :P")';
+      echo '</script>';   
+      header("Refresh: 1; url=signin.php"); 
+      exit();
+	}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,8 +19,14 @@
 <body>
 
 
-Welcome
+Welcome 
+<?php
+
+echo $_SESSION['login_user']; ?>
+<br>
 <a href="logout.php">logout</a>
+<br>
+<a href="layout.html">Make new Issue</a>
 
 </body>
 </html>
