e56c94b3f0fe709980b9453505fa4b1014827480
ASWANTH K <aswanthkoleri@users.noreply.github.com>
2017-03-25 05:50:34 +0530

    added Signup functionality

--- signin.php
+++ signin.php
@@ -55,25 +55,27 @@
 			</form>
 			</div>
 			<div class="sign-up-htm">
+				<form class="login-form" method="POST" action="signup.php">
 				<div class="group">
 					<label for="user" class="label">Username</label>
-					<input id="user" type="text" class="input">
+					<input id="user" type="text" class="input" name="username1">
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
+					<input id="pass" type="password" class="input" data-type="password" name="password1">
 				</div>
 				<div class="group">
-					<label for="pass" class="label">Repeat Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
+					<label for="phone" class="label">Phone</label>
+					<input id="phone" type="text" class="input" data-type="password" name="phone">
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Email Address</label>
-					<input id="pass" type="text" class="input">
+					<input id="pass" type="text" class="input" name="email">
 				</div>
 				<div class="group">
 					<input type="submit" class="button" value="Sign Up">
 				</div>
+				</form>
 				<div class="hr"></div>
 				<div class="foot-lnk">
 					<label for="tab-1">Already Member?</a>
